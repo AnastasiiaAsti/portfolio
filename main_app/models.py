b2e08e28db598498c536ca1124539c68d2aac796
Asti <astistateofmind@gmail.com>
2022-07-15 14:56:22 -0700

    add links and portfolio model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.URLField(max_length=200)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f'{self.title} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
