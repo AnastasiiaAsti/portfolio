f36fb0ee724ce0f4745a8e97a62fd514ad29990d
Asti <astistateofmind@gmail.com>
2022-07-15 15:16:15 -0700

    routed all urls

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -7,6 +8,15 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     link = models.URLField(max_length=200)
     description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField('post date')
+    details = models.CharField(max_length=2000)
 
     def __str__(self):
         return f'{self.title} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .models import Project, Post
 # Create your views here.
 
 
@@ -14,3 +15,13 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def projects_index(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/index.html', {'projects': projects})
+
+
+def posts_index(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/index.html', {'posts': posts})
