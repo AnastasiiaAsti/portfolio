9bae0cfc2858cddeb5aab055dba7ecac63b87225
Asti <astistateofmind@gmail.com>
2022-07-20 17:35:43 -0700

    add img model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, Post, Skill, Photo
+from .models import Project, Post, Skill, Photo, Picture
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Project)
 admin.site.register(Post)
 admin.site.register(Skill)
 admin.site.register(Photo)
+admin.site.register(Picture)
--- main_app/models.py
+++ main_app/models.py
@@ -35,3 +35,11 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"Photo for project_id: {self.project_id} @{self.url}"
+
+
+class Picture(models.Model):
+    url = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for post_id: {self.post_id} @{self.url}"
