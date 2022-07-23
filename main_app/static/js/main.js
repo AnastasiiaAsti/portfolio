c883e6e921cf5222c8c19f04e92755388930583a
Asti <astistateofmind@gmail.com>
2022-07-23 14:51:53 -0700

    add style

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -2,4 +2,3 @@ let colors = ['#F8B195', '# F67280', '#C06C84 ', '#6C5B7B', '#355C7D', '#9DE0AD'
     '#FE4365', '#F9CDAD'];
 
 let random_color = colors[Math.floor(Math.random() * colors.length)];
-document.querySelectorAll('.skill').style.color = random_color;
