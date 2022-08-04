374490ccfbb43045f50d068d9ee23ef989f8b535
Asti <astistateofmind@gmail.com>
2022-08-04 12:43:38 -0700

    add github icon to projects

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,7 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     link = models.URLField(max_length=200)
     description = models.CharField(max_length=1000)
+    linkGh = models.URLField(max_length=200)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
